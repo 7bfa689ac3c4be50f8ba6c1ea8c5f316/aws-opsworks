b234b606960dc6125d87ea2f412793cd0745d7ab
Tomas Benedykt <tomas@mamlas.net>
2015-05-12 16:43:16 +0200

    improve force_ssl_on_elb recipe in nginx cookbook

--- nginx/recipes/force_ssl_on_elb.rb
+++ nginx/recipes/force_ssl_on_elb.rb
@@ -7,4 +7,4 @@
 Chef::Log.info("Starting force_ssl_on_elb ... node=#{node[:nginx][:force_ssl_on_elb]}")
 node.force_default[:nginx][:force_ssl_on_elb] = true
 Chef::Log.info("Starting 2nd force_ssl_on_elb ... node=#{node[:nginx][:force_ssl_on_elb]}")
-include_recipe "nginx"
+include_recipe "wrap_nginx"
